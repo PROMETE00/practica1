28dd12c965c741589be38fec7ce6c279a3a1ffcd
Prometeo Mendoza Luis <prometeomendoza27@gmail.com>
2024-03-01 19:17:05 -0600

    Equipo3

---
+++ practica1.py
@@ -0,0 +1,6 @@
+"""Dado un texto, identificar que se trata de un comentario de 
+múltiples líneas, el cual consiste en una cadena rodeada por /* y 
+*/, sin un */ entre ellos, a
+menos que se encierre entre dobles comillas (")."""
+
+
