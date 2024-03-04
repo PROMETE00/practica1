6f0be6c9ae1fd009a02d261a1d47cd9867dcb9aa
Prometeo Mendoza Luis <prometeomendoza27@gmail.com>
2024-03-04 16:51:04 -0600

    deleted:    practica1.py

--- practica1.py
+++
@@ -1,6 +0,0 @@
-"""Dado un texto, identificar que se trata de un comentario de 
-múltiples líneas, el cual consiste en una cadena rodeada por /* y 
-*/, sin un */ entre ellos, a
-menos que se encierre entre dobles comillas (")."""
-
-
